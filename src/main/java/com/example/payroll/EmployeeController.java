7f1c3fd0c10ffc0a2e0c873e7ece0ee1ce8ae2c4
sangwopark19 <sangwopark19@gmail.com>
2023-09-27 14:32:34 +0900

    REST Service 프로젝트 생성 및 초기화

---
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.example.payroll;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    // Aggregate root
+    // tag::get-aggregate-root[]
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow();
+
+//        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+}
