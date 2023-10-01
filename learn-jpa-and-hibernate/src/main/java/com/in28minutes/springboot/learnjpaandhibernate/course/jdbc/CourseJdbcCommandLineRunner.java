1ef1dd916cb8c2f43aae54e8ddde1bbed8d0be39
sangwopark19 <sangwopark19@gmail.com>
2023-10-01 19:15:17 +0900

    jpa-hibernate 추가 및 jdbc 설정

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,22 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
+
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+    @Autowired
+    private CourseJdbcRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        repository.insert(new Course(1, "Learn now!", "in28min"));
+        repository.insert(new Course(2, "asdf now!", "in28min"));
+        repository.insert(new Course(3, "asdfddd now!", "in28min"));
+
+        repository.deleteBydId(1);
+    }
+
+
+}
