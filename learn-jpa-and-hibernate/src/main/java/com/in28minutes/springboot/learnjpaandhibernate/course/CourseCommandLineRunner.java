432f8586b7401b48b716b01487b72c52a2845b16
sangwopark19 <sangwopark19@gmail.com>
2023-10-01 20:07:54 +0900

    jap -> springDataJpa로 변경

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.learnjpaandhibernate.course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,18 +12,25 @@ import org.springframework.stereotype.Component;
 public class CourseCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
+
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn Jpa!", "in28min"));
-        repository.insert(new Course(2, "jpa now!", "in28min"));
-        repository.insert(new Course(3, "asdfddd now!", "in28min"));
+        // 그전에는 직접만든 repository.insert를 사용했지만
+        // springDataJpa 는 기본적으로 상속받은 메서드를 사용한다
+        repository.save(new Course(1, "Learn Jpa!", "in28min"));
+        repository.save(new Course(2, "jpa now!", "in28min"));
+        repository.save(new Course(3, "asdfddd now!", "in28min"));
 
-        repository.deleteById(1);
+        repository.deleteById(1L);
 
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        System.out.println(repository.findById(2L));
+        System.out.println(repository.findById(3L));
+
     }
 
 
