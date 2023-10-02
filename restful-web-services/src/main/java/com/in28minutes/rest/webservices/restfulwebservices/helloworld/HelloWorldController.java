312a2338e4c99a478fb22d707e039cddcadb45ef
sangwopark19 <sangwopark19@gmail.com>
2023-10-02 15:10:57 +0900

    hello-world controller add

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+
+        return "Hello World";
+    }
+}
