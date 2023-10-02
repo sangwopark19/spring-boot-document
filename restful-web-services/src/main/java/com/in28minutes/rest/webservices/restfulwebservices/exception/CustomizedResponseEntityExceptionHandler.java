a451686fb61d087fe069d0b5a4a26f192fef9133
sangwopark19 <sangwopark19@gmail.com>
2023-10-02 20:03:50 +0900

    모든 리소스를 대상으로 예외처리 구현하기 스프링의 기본 익셉션 핸들러인 ResponseEntityExceptionHandler를상속받아 CustomizedResponseEntityExceptionHandler 클래스를 생성했다. 여기서 직접만든 ErrorDetails 객체에 각종 에러정보들을 담아 response status와 함께 보냈다.

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,4 +1,35 @@
 package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
-public class CustomizedResponseEntityExceptionHandler {
+import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+//이 클래스를 모든 컨트롤러에게 적용한다는 어노테이션
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    // 모든 익셉션은 기본적으로 이 핸들러로 들어가 INTERNAL_SERVER_ERROR 를 보낸다
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    // 하지만 UserNotFoundException 클래스에서 일어난 익셉션은 NOT_FOUND 를 보낸다
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
 }
