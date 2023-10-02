6ac37c2606c49c566a241298458c5b09fee65c39
sangwopark19 <sangwopark19@gmail.com>
2023-10-02 17:36:37 +0900

    restapi putmapping 201 반환 및 location 설정 추가

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,7 +29,20 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        //ResponseEntity.created() 에 줄 매개변수
+        //ServletUriComponentsBuilder.fromCurrentRequest 는 현재 메소드의 경로를가져온다 ex. /users
+        //path()는 추가할 path를 설정한다. ex. /users/{id}
+        //buildAndExpand()는 path에서 설정할 {id}를 유동적으로 방금 생성한 사용자의 getId로 바꿔준다. ex. /users/id{4}
+        //toUri로 uri형식으로 바꿔준다.
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        // ResponseEntity.created()는 http 201을 반환한다
+        //그리고 매개변수로 uri 를 주면 해당 uri를 사용자에게 보내준다.
+        return ResponseEntity.created(location).build();
     }
 }
