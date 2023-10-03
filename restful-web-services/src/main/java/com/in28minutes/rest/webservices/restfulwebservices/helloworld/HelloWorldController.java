a02c58238a9af895db72dfb6c6fca8178bea90aa
sangwopark19 <sangwopark19@gmail.com>
2023-10-03 17:58:01 +0900

    Accept-Language 헤더를 기반으로 restapi 국제화 messages.properties 와 LocalContext	Holder 활용

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,15 +1,29 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    @Autowired
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+
 
     @GetMapping("/hello-world")
+
     public String helloWorld() {
         return "Hello World";
     }
@@ -24,5 +38,12 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format(
                 "Hello World %s", name
         ));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+//        return "Hello World";
     }
 }
