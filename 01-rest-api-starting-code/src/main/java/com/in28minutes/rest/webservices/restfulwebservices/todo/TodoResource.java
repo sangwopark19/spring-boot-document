64023b07aca9ae0358d22fa644d83018d9771072
Sangwoo Park <sangwopark19@gmail.com>
2023-12-16 20:14:27 +0900

    rest api todo 생성 및 업데이트 기능 구현

--- 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,5 +37,18 @@ public class TodoResource {
     return ResponseEntity.noContent().build();
   }
 
+  @PutMapping("/users/{username}/todos/{id}")
+  public Todo updateTodo(@PathVariable String username, @PathVariable int id,
+      @RequestBody Todo todo) {
+    todoService.updateTodo(todo);
+    return todo;
+  }
+
+  @PostMapping("/users/{username}/todos")
+  public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) {
+    Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(),
+        todo.isDone());
+    return createdTodo;
+  }
 
 }
